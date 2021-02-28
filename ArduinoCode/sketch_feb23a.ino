45cdf0bc565e2362303f35713e7ad1b0eabe5de6
SezerUrun <30298593+SezerUrun@users.noreply.github.com>
2021-02-28 13:54:54 +0300

    Update sketch_feb23a.ino

--- ArduinoCode/sketch_feb23a.ino
+++ ArduinoCode/sketch_feb23a.ino
@@ -2,16 +2,19 @@
 
 SoftwareSerial bluetoothModule(10, 11); 
 
-const int led0=13;
-const int leftEngine=2;
-const int rightEngine=3;
+const int leftEngineForward=2;
+const int leftEngineBack=3;
+const int rightEngineForward=4;
+const int rightEngineBack=5;
 
 void setup()
 {
   bluetoothModule.begin(9600);
   pinMode(led0,OUTPUT);
-  pinMode(leftEngine, OUTPUT);
-  pinMode(rightEngine, OUTPUT);
+  pinMode(leftEngineForward, OUTPUT);
+  pinMode(leftEngineBack, OUTPUT);
+  pinMode(rightEngineForward, OUTPUT);
+  pinMode(rightEngineBack, OUTPUT);
 }
 
 char character;
@@ -20,25 +23,40 @@ void loop()
   while(bluetoothModule.available()>0){
     character = bluetoothModule.read();
     switch(character){ 
-      case '0':
-        digitalWrite(leftEngine, HIGH);
-        digitalWrite(rightEngine, HIGH);
-        bluetoothModule.println("go");
+      case '0': //GO FORWARD
+        digitalWrite(leftEngineForward, HIGH);
+        digitalWrite(rightEngineForward, HIGH);
+        digitalWrite(leftEngineBack, LOW);
+        digitalWrite(rightEngineBack, LOW);
+        bluetoothModule.println("go forward");
         break;
-      case '1':
-        digitalWrite(leftEngine, LOW);
-        digitalWrite(rightEngine, LOW);
-        bluetoothModule.println("stop");
+      case '1': //GO BACK
+        digitalWrite(leftEngineForward, LOW);
+        digitalWrite(rightEngineForward, LOW);
+        digitalWrite(leftEngineBack, HIGH);
+        digitalWrite(rightEngineBack, HIGH);
+        bluetoothModule.println("go back");
         break; 
-      case '2':
-        digitalWrite(leftEngine, LOW);
-        digitalWrite(rightEngine, HIGH);
+      case '2': //TURN LEFT
+        digitalWrite(leftEngineForward, LOW);
+        digitalWrite(rightEngineForward, HIGH);
+        digitalWrite(leftEngineBack, HIGH);
+        digitalWrite(rightEngineBack, LOW);
         bluetoothModule.println("turn left");
         break;  
-      case '3':
-        digitalWrite(leftEngine, HIGH);
-        digitalWrite(rightEngine, LOW);
+      case '3': //TURN RIGHT
+        digitalWrite(leftEngineForward, HIGH);
+        digitalWrite(rightEngineForward, LOW);
+        digitalWrite(leftEngineBack, LOW);
+        digitalWrite(rightEngineBack, HIGH);
         bluetoothModule.println("turn right");
+        break;
+      case '4': //STOP
+        digitalWrite(leftEngineForward, LOw);
+        digitalWrite(rightEngineForward, LOW);
+        digitalWrite(leftEngineBack, LOW);
+        digitalWrite(rightEngineBack, LOW);
+        bluetoothModule.println("stop");
         break; 
     }
   }
